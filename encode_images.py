8166bc2c9085cd7bebd8d3d4a62ad3185b7920f1
mostafa hassan <mostafahassan999888@gmail.com>
2020-10-13 22:15:49 +0200

    Revert "Changes the lists to dicts"
    This reverts commit 5726bdbbacf016795f78fb413dcf8cb6509dc031.

--- encode_images.py
+++ encode_images.py
@@ -1,3 +1,4 @@
+import cv2
 import face_recognition
 import os
 
@@ -5,26 +6,25 @@ def load_images(path_to_images):
     # takes the path of the students images to load them
     # returns a list of those images and the corresponding student's name
     images = []
-    students_names = []
+    students_name = []
     images_name = os.listdir(path_to_images)
 
     for image_name in images_name:
         images.append(face_recognition.load_image_file(f'{path_to_images}/{image_name}'))
         # to remove the image extension
-        students_names.append(os.path.splitext(image_name)[0])
-    return images, students_names
+        students_name.append(os.path.splitext(image_name)[0])
+    return images, students_name
 
-def encode_images(path_to_images):
-    # takes the path of the students images to load them
-    # returns dict of students names and their encodings
-    images, students_names = load_images(path_to_images)
-    encodings = {}
-    for img, student_name in zip(images,students_names):
+def encode_images(images):
+    # takes the Student images we want to encode
+    # returns the encodings
+    encodings = []
+    for img in images:
         encode = face_recognition.face_encodings(img)
-        encodings[student_name]= encode
+        encodings.append(encode)
     return encodings
 
 
 path_to_images = "Resources/Student_images"
-encodings= encode_images(path_to_images)
-print(encodings)
+images, students_name =load_images(path_to_images)
+encodings = encode_images(images)
