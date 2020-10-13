da3d47aedb35e6f3ffe720d96ffeb7eed47fd6e4
mostafa hassan <mostafahassan999888@gmail.com>
2020-10-13 21:33:43 +0200

    Added encode_images function, edited load_images function
    edited the load_images function  to make it extract the student name from the image file name and return them

--- encode_images.py
+++ encode_images.py
@@ -4,15 +4,27 @@ import os
 
 def load_images(path_to_images):
     # takes the path of the students images to load them
-    # returns a list of those images
+    # returns a list of those images and the corresponding student's name
     images = []
-
+    students_name = []
     images_name = os.listdir(path_to_images)
 
     for image_name in images_name:
-        images.append(cv2.imread(f'{path_to_images}/{image_name}'))
+        images.append(face_recognition.load_image_file(f'{path_to_images}/{image_name}'))
+        # to remove the image extension
+        students_name.append(os.path.splitext(image_name)[0])
+    return images, students_name
 
-    return images
+def encode_images(images):
+    # takes the Student images we want to encode
+    # returns the encodings
+    encodings = []
+    for img in images:
+        encode = face_recognition.face_encodings(img)
+        encodings.append(encode)
+    return encodings
+
 
 path_to_images = "Resources/Student_images"
-load_images(path_to_images)
+images, students_name =load_images(path_to_images)
+encodings = encode_images(images)
