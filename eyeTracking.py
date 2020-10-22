5332573411c5787075c1ba0da8491ba5c3e9b9e7
amiraElmergawy <49119157+amiraElmergawy@users.noreply.github.com>
2020-10-22 22:23:40 +0200

    Update eyeTracking.py

--- eyeTracking.py
+++ eyeTracking.py
@@ -33,13 +33,11 @@ def contouring(thresh, mid, img, right=False):
         if right:
             cx += mid  # Adding value of mid to x coordinate of centre of
             # right eye to adjust for dividing into two parts
-        cv2.circle(img, (cx, cy), 1, (0, 255, 0), -1)  # drawing over eyeball with green
+        cv2.circle(img, (cx, cy), 1, (0, 255, 0), 3)  # drawing over eyeball with green
         return cy, cx
     except:
         pass  # occurs when eyes are not detected
 
-
-#def calc_eye_
 
 
 def get_eye_ball_center(list, right=False, x=False):
@@ -61,44 +59,27 @@ def get_eye_ball_center(list, right=False, x=False):
             coordinate += list[x][c]
             x = x + 1
 
-    # print('list entered element: ' + coordinate)
 
     coordinate = coordinate // 6
 
-    # print('coordinate: ' + str(coordinate))
     return coordinate
 
-
-def compare_eye_center_positions(x1, y1, x2, y2):
-    print('x1: '+str(x1))
-    print('x2: ' + str(x2))
-    print('y1: ' + str(y1))
-    print('y2: ' + str(y2))
-
-    img1 = cv2.imread('Resources/cheater.jpg')
-    if (x1 == x2) or (y1 == y2):
-        cv2.destroyWindow('cheater image')
-    else:
-        cv2.imshow('cheater image', img1)
-        cv2.waitKey(0)
 
 
 
 def compare_eyeballs(y_left, x_left, y_right, x_right, y_center, x_center,img):
     try:
-        diff_x= abs(x_left-x_right)/4
+        diff_x= abs(x_left-x_right)/3
         print("diff ", diff_x)
         print('left point coord: ' + str(x_left) +','+str(y_left))
         print('right point coord: ' + str(x_right)+','+ str(y_right))
         print('center point coord: ' + str(x_center)+','+str(y_center))
         cv2.putText(img,'left : '+str(x_left)+"  center : "+ str(x_center)+ "  right : "+str(x_right),(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),1)
         img1 = cv2.imread('Resources/cheater.jpg')
+        cv2.imshow("eyes", img)
         if x_center-diff_x <= x_left:
             cv2.imshow('cheater image looking left', img1)
             cv2.waitKey(0)
-       # elif (y_center+ 7<= y_left) or (y_center+7 <= y_right):
-        #    cv2.imshow('cheater image looking up', img1)
-         #   cv2.waitKey(0)
         elif x_center+diff_x >= x_right:
             cv2.imshow('cheater image looking right', img1)
             cv2.waitKey(0)
@@ -116,15 +97,11 @@ right = [42, 43, 44, 45, 46, 47]  # keypoint indices for right eye
 
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
-# thresh = img.copy()
 
 cv2.namedWindow('image')
 kernel = np.ones((20, 20), np.uint8)  # used for dilation of eyes in the mask
 process_frame = False
-# def nothing(x):
-#    pass
-#
-# cv2.createTrackbar('threshold', 'image', 0, 255, nothing)
+
 
 while (True):
     image2 = np.zeros((1000, 1000, 3), np.uint8)
@@ -135,12 +112,7 @@ while (True):
         rects = detector(gray, 1)  # contains all the faces detected
         # so we need to save only the person that i must track his eyes istead of saving all the faces
         # if the previous step done we will never need for-loop as we will have one face
-        # print('# of detected faces: ' + str(rects))
-        # rectP1 = rects.rectangles[0][0]
-        # rectP2 = rects.rectangles[0][1]
-        # print('rect values: ' + str(rectP1) + str(rectP2))
-        # cv2.rectangle(img, rects[0][0], rects[0][1], (0, 0, 255), 3)
-        # faceBoxRectangleS = tuple of dlib.rectangle(left=someNewLeftValue, top=someNewTopValue, right=someNewRightValue, bottom=someNewBottomValue)
+
         for rect in rects:
             shape = predictor(gray, rect)  # to predict all face objects
             shape = shape_to_np(shape)  # to convert all face objects' keypoints to (x, y)-coordinates
@@ -156,9 +128,7 @@ while (True):
             eyes[mask] = [255, 255, 255]
             mid = (shape[42][0] + shape[39][0]) // 2
             eyes_gray = cv2.cvtColor(eyes, cv2.COLOR_BGR2GRAY)
-            #threshold = cv2.getTrackbarPos('threshold', 'image')
             threshold = 152
-            # print('threshold value is: ' + str(threshold))
             _, thresh = cv2.threshold(eyes_gray, threshold, 255, cv2.THRESH_BINARY)
             thresh = cv2.erode(thresh, None, iterations=2)  # 1
             thresh = cv2.dilate(thresh, None, iterations=4)  # 2
@@ -174,17 +144,13 @@ while (True):
             image2[shape[39][1], shape[39][0]] = (200, 0, 200)
             image2[shape[40][1], shape[40][0]] = (200, 0, 200)
             image2[shape[41][1], shape[41][0]] = (200, 0, 200)
-            # eye_center_x = get_eye_ball_center(shape, False, True)
-            # eye_center_y = get_eye_ball_center(shape)
-            # image2[eye_center_x, eye_center_y] = (0, 0, 255)
-            # print('place of forward ceneter: ' + str((eye_center_x, eye_center_y)))
             image2[hmadaLeft] = (255, 0, 0)
-            # print('place of real time ceneter: ' + str(hmadaLeft))
+
             try:
                 compare_eyeballs(shape[36][1], shape[36][0], shape[39][1], shape[39][0], hmadaLeft[0], hmadaLeft[1],img)
             except:
                pass
-            # compare_eye_center_positions(eye_center_x, eye_center_y, hmadaLeft[0], hmadaLeft[1])
+
 
              # Change one pixel to draw right eye
             image2[shape[42][1], shape[42][0]] = (200, 0, 200)
@@ -193,30 +159,18 @@ while (True):
             image2[shape[45][1], shape[45][0]] = (200, 0, 200)
             image2[shape[46][1], shape[46][0]] = (200, 0, 200)
             image2[shape[47][1], shape[47][0]] = (200, 0, 200)
-             # eye_center_x = get_eye_ball_center(shape, True, True)
-             # eye_center_y = get_eye_ball_center(shape, True)
-             # image2[eye_center_x, eye_center_y] = (0, 0, 255)
-             # print('place of forward ceneter: ' + str((eye_center_x, eye_center_y)))
             image2[hmadaRight] = (255, 0, 0)
-            # print('place of real time ceneter: ' + str(hmadaRight))
-            # compare_eye_center_positions(eye_center_x, eye_center_y, hmadaRight[0], hmadaRight[1])
             try:
                 compare_eyeballs(shape[42][1], shape[42][0], shape[45][1], shape[45][0], hmadaRight[0], hmadaRight[1],img)
             except:
                 pass
-            # for (x, y) in shape[36:48]:
-            #     cv2.circle(img, (x, y), 2, (255, 0, 0), -1)
         process_frame = False
     # show the image with the face detections + facial landmarks
 
-    # Save
-    # cv2.imwrite("result.png", image2)
-    # cv2.imshow('eyePoints', image2)
-
-    cv2.imshow('eyes', img)
+    # cv2.imshow('eyes', img)
     cv2.destroyWindow('image')
     cv2.destroyWindow('eyePoints')
-    # cv2.imshow("image", thresh)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
